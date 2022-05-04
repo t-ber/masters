2d92c072bd6cbffe9d3b79da681e0b7254f85350
torbjørn <torbjorn1997@hotmail.no>
2022-05-04 21:56:09 +0200

    gonna try to implement RNN now

--- main.py
+++ main.py
@@ -1,118 +1,53 @@
+from operator import mod
 from select import select
+from statistics import mode
 from this import d
+import time
 from scipy import rand
 import torch
 import scipy.io
 import matplotlib.pyplot as plt
 import numpy as np
 from torch import batch_norm, double, nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import TensorDataset
 from torch_geometric.data import Data
 import torch.nn.functional as F
+from torch_geometric.loader import DataLoader
 from torch_geometric.nn import GCNConv, BatchNorm, ChebConv, TransformerConv, AGNNConv, TAGConv, GINConv ,GINEConv, ARMAConv, SGConv, APPNP, MFConv
 from torch_geometric.data import InMemoryDataset, download_url
 from tqdm import tqdm
 import random
 
 from torch_geometric_temporal.nn.recurrent import GConvGRU
+from train import *
 
+print("Making dataset")
 
+data, targets = load_dataset('Indian_pines')
 
+train_data, train_targets = split_training_data(data, targets, 0.3)
 
-def make_edge_indexes(all_edges, number_of_nodes):
-    edge_index = []
+train_data = make_training_data(train_data, train_targets, 3, 0)
 
-    if all_edges:
-        for node in range(number_of_nodes):
-            for node_to_connect in range(node+1,node+2):
-                if node_to_connect == number_of_nodes:
-                    break
-                edge_to = [node, node_to_connect]
-                edge_from = [node_to_connect, node]
-                edge_index.append(edge_to)
-                edge_index.append(edge_from)
+test_data = make_test_data(data, targets)
 
-    """elif not all_edges:
-        for node in range(number_of_nodes - 1):
-            edge_to = [node, node+1]
-            edge_back = [node+1, node]
-            edge_index.append(edge_to)
-            edge_index.append(edge_back)"""
+train_dataloader = DataLoader(train_data, batch_size=24, shuffle=True)
 
-    return torch.tensor(edge_index, dtype=torch.long)
-
-
-raw_data = scipy.io.loadmat('data/indian_pines.mat')
-ground_truth = scipy.io.loadmat('data/indian_pines_gt.mat')
-dataset = raw_data['indian_pines'] # use the key for data here
-target = ground_truth['indian_pines_gt'] # use the key for target here
-
-dataset = dataset.astype(int)
-target = target.astype(int)
-
-
+test_dataloader = DataLoader(test_data)
 #plt.imshow(target)
 #plt.show()
 
-
-edge_index = make_edge_indexes(True, 145)
-
-print(dataset[5])
-datalist = []
-for i in range(145):
-    spectra = torch.from_numpy(dataset[i]).float()
-    spectra_target = torch.from_numpy(target[i]).long()# change type to your use case
-
-    data = Data(x=spectra, edge_index=edge_index.t().contiguous(), y=spectra_target)
-    datalist.append(data)
-
-
-
+print("Initiating training")
 #dataloader_iterator = iter(train_dataloader)
 
 #data, target = next(dataloader_iterator)
-print(len(datalist))
+
 #convert to graph
 #loader = DataLoader(datalist, shuffle=False)
 
 
 m = 0.8
 
-"""class GCN(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = GCNConv(220, 220)
-        self.conv2 = GCNConv(220, 150)
-        self.conv3 = GCNConv(150, 100)
-        self.conv4 = GCNConv(100, 60)
-        self.conv5 = GCNConv(60, 30)
-        self.conv6 = GCNConv(30, 17)
-        self.batch_norm1 = BatchNorm(220, eps=1e-5, momentum=0.9)
-        self.batch_norm2 = BatchNorm(150, eps=1e-5, momentum=0.9)
-        self.batch_norm3 = BatchNorm(100, eps=1e-5, momentum=0.9)
-        self.batch_norm4 = BatchNorm(60, eps=1e-5, momentum=0.9)
-        self.batch_norm5 = BatchNorm(30, eps=1e-5, momentum=0.9)
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
-
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = self.batch_norm1(x)
-        x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
-        x = self.batch_norm2(x)
-        x = self.conv3(x, edge_index)
-        x = F.relu(x)
-        x = self.batch_norm3(x)
-        x = self.conv4(x, edge_index)
-        x = F.relu(x)
-        x = self.batch_norm4(x)
-        x = self.conv5(x, edge_index)
-        x = F.relu(x)
-        x = self.batch_norm5(x)
-        x = self.conv6(x, edge_index)
-        return F.log_softmax(x, dim=1)"""
     
 def make_conv_layer(inputs, outputs):
     return TransformerConv(inputs, outputs)
@@ -121,22 +56,25 @@ class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         """super(GCN, self).__init__()"""
+
+
         self.hidden = GConvGRU(220, 220, 1)
         
 
-        self.linear1 = nn.Linear(60, 30)
-        self.linear2 = nn.Linear(30,17)
+        self.linear1 = nn.Linear(60, 60)
+        self.linear2 = nn.Linear(60, 30)
+        self.linear3 = nn.Linear(30, 4)
 
         self.conv1 = make_conv_layer(220, 150)
-        self.conv2 = make_conv_layer(150, 100)
+        self.conv2 = make_conv_layer(150, 60)
 
-        self.conv3 = make_conv_layer(100, 60)
+        self.conv3 = make_conv_layer(60, 30)
         
         self.batch_norm1 = BatchNorm(150, eps=1e-5, momentum=0.9)
-        self.batch_norm2 = BatchNorm(100, eps=1e-5, momentum=0.9)
+        self.batch_norm2 = BatchNorm(60, eps=1e-5, momentum=0.9)
         self.batch_norm3 = BatchNorm(60, eps=1e-5, momentum=0.9)
        
-    def forward(self, data):
+    def forward(self, data, hidden):
         x, edge_index = data.x, data.edge_index
         
         x = self.conv1(x, edge_index)
@@ -146,17 +84,37 @@ class GCN(torch.nn.Module):
         
         x = self.conv2(x, edge_index)
         x = self.batch_norm2(x)
-      
-        x = F.relu(x)
-        
-        x = self.conv3(x, edge_index)
-        x = self.batch_norm3(x)
-        
-        
-        
-        
-        return F.log_softmax(x, dim=1)
 
+        combined = torch.cat((x, hidden)
+        hidden = x
+
+        x = F.relu(combined)
+        
+        #x = self.conv3(x, edge_index)
+        #x = self.batch_norm3(x)
+        
+        #x = F.relu(x)
+
+        x = self.linear1(x)
+        x = self.batch_norm3(x)
+        x = F.relu(x)
+
+        x = self.linear2(x)
+        x = F.relu(x)
+
+        x = self.linear3(x)
+        
+        
+        return F.log_softmax(x, dim=1), hidden
+
+
+
+training_spectra = [8, 9, 10, 22, 23, 24, 32, 33, 34, 44, 45, 46, 52, 53, 54, 
+                    65, 66, 67, 69, 70, 71, 79, 80, 81, 
+                    99, 100, 101, 118, 119, 120, 132, 133, 138, 139, 140]
+
+################################
+#Training section
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = GCN().to(device)
@@ -164,64 +122,38 @@ model = GCN().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 loss = 100
 best_loss = 100
-
-training_spectra = [8, 9, 10, 22, 23, 24, 32, 33, 34, 44, 45, 46, 52, 53, 54, 
-                    65, 66, 67, 69, 70, 71, 79, 80, 81, 
-                    99, 100, 101, 118, 119, 120, 132, 133, 138, 139, 140]
-
+times_no_new_best_loss = 0
 model.train()
-pbar = tqdm(range(200))
-number_of_spectra = len(training_spectra)
-n = 0
 
-spectra_number_list = training_spectra
-number_of_spectra = 145
-spectra_number_list = list(range(number_of_spectra))
+number_of_epochs = 20
+for i in range(number_of_epochs):
+    avg_loss = train_loop(train_dataloader, model, optimizer, device)
+    avg_loss = avg_loss.data.cpu().numpy()
+    print(avg_loss)
+    if avg_loss < best_loss:
+        times_no_new_best_loss = 0
+        best_loss = avg_loss
+        print("Saving model")
+        torch.save(model.state_dict(), "best_model")
+    else:
+        times_no_new_best_loss += 1
+    if times_no_new_best_loss == 30:
+        break
+################################
 
-for epoch in pbar:
-    #random.shuffle(spectra_number_list)
-    #print(spectra_number_list)
-    for spectra_number in spectra_number_list:
-        data = datalist[spectra_number]
-        data.to(device)
-        pbar.set_description("Loss: %f" % loss)
-        optimizer.zero_grad()
-        out = model(data)
-        loss = F.nll_loss(out, data.y)
-        if loss < best_loss:
-            torch.save(model.state_dict(), "best_model")
-            best_loss = loss
-        loss.backward()
-        optimizer.step()
-        """n += 1
-        if n == number_of_spectra:
-            n = 0"""
-        
 
 model = GCN().to(device)
-model.load_state_dict(torch.load("best_model"))
-model.eval()
-correct = 0
-predicted_image = []
 
-for spectra in range(number_of_spectra):
-    data = datalist[spectra]
-    data.to(device)
-    pred = model(data).argmax(dim=1)
-    correct += (pred == data.y).sum()
-    pred = pred.tolist()
-    predicted_image.append(pred)
-print("wrongs: ", len(data.y)*number_of_spectra - int(correct))
-acc = int(correct) / int((len(data.y)*number_of_spectra))
-print(f'Accuracy: {acc:.4f}')
+accuracy, predicted_image = test_loop(test_dataloader, model, device)
+
 
 #predicted_image = predicted_image.cpu()
-data.y = data.y.cpu()
+
 
 
 f, axarr = plt.subplots(1,2)
 axarr[0].imshow(predicted_image)
-axarr[1].imshow(target)
+axarr[1].imshow(targets)
 
 plt.show()
 
--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 from select import select
 from this import d
+
+from matplotlib import transforms
 from scipy import rand
 import torch
 import scipy.io
 import matplotlib.pyplot as plt
 import numpy as np
 from torch import batch_norm, double, nn
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import TensorDataset
 from torch_geometric.data import Data
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, BatchNorm, ChebConv, TransformerConv, AGNNConv, TAGConv, GINConv ,GINEConv, ARMAConv, SGConv, APPNP, MFConv
@@ -17,8 +19,131 @@ import random
 from torch_geometric_temporal.nn.recurrent import GConvGRU
 
 
-def make_
+def load_dataset(name):
+    datapath = 'data/' + name + '.mat'
+    ground_truth_path = 'data/' + name + '_class.mat'
+    raw_data = scipy.io.loadmat(datapath)
+    ground_truth = scipy.io.loadmat(ground_truth_path)
+    #name = name.lower()
+    dataset = raw_data[name] # use the key for data here
+    gt_key = name + '_class'
+    target = ground_truth[gt_key] # use the key for target here
+
+    dataset = dataset.astype(int)
+    target = target.astype(int)
+
+    return dataset, target
+
+def split_training_data(data, targets, training_size):
+    number_of_training_samples = np.round(training_size*len(data)).astype(int)
+    train_indexes = np.round(np.linspace(0, len(data) - 1, number_of_training_samples)).astype(int)
+    
+    
+    train_data = []
+    train_targets = []
+
+    for index in train_indexes:
+        train_data.append(data[index])
+        train_targets.append(targets[index])
 
 
-def train_batch():
-    pass
+    return train_data, train_targets
+
+
+def make_edge_indexes(number_of_nodes):
+    edge_index = []
+
+    for node in range(number_of_nodes - 1):
+        edge_to = [node, node+1]
+        edge_back = [node+1, node]
+        edge_index.append(edge_to)
+        edge_index.append(edge_back)
+
+    return torch.tensor(edge_index, dtype=torch.long)
+
+
+def make_training_data(raw_data, target, nodes_in_data, node_overlap):
+
+    edge_indexes = make_edge_indexes(nodes_in_data)
+      
+    datalist = []
+   
+    for i in range(len(raw_data)):
+        
+        spectra = torch.from_numpy(raw_data[i]).float()
+        spectra_target = torch.from_numpy(target[i].flatten()).long()# change type to your use case
+        
+        n = 0
+        break_loop  = False
+        while(not break_loop):
+            if n == len(spectra):
+                break
+            while n + nodes_in_data >= len(spectra):
+                n -= 1
+                break_loop = True
+
+            mini_spectra = spectra[n:n+nodes_in_data]
+            mini_spectra_target = spectra_target [n:n+nodes_in_data]
+            data = Data(x=mini_spectra, edge_index=edge_indexes.t().contiguous(), y=mini_spectra_target)
+            datalist.append(data)
+        
+            n += nodes_in_data
+            n -= node_overlap
+
+    return datalist
+
+def make_test_data(raw_data, targets):
+    nodes_in_data = len(raw_data[0])
+    edge_indexes = make_edge_indexes(nodes_in_data)
+
+    data_list = []
+
+    for i in range(len(raw_data)):
+        
+        spectra = torch.from_numpy(raw_data[i]).float()
+        spectra_target = torch.from_numpy(targets[i].flatten()).long()# change type to your use case
+
+        data = Data(x=spectra, edge_index=edge_indexes.t().contiguous(), y=spectra_target)
+        data_list.append(data)
+        
+    return data_list
+
+
+def train_loop(data_loader, model, optimizer, device):
+    size = len(data_loader.dataset)
+    summed_loss = 0
+    for batch, data in tqdm(enumerate(data_loader)):
+        data.to(device)
+        #pbar.set_description("Loss: %f" % loss)
+        out = model(data)
+        loss = F.nll_loss(out, data.y)
+        summed_loss += loss
+        """if loss < best_loss:
+            torch.save(model.state_dict(), "best_model")
+            best_loss = loss"""
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+    avg_loss = summed_loss/size
+    return avg_loss
+
+
+def test_loop(data_loader, model, device):
+    size = len(data_loader.dataset)
+    model.load_state_dict(torch.load("best_model"))
+    model.eval()
+    correct = 0
+    predicted_image = []
+
+    for batch, data in tqdm(enumerate(data_loader)):
+        data.to(device)
+        pred = model(data).argmax(dim=1)
+        correct += (pred == data.y).sum()
+        pred = pred.tolist()
+        predicted_image.append(pred)
+    print("wrongs: ", len(data.y)*size - int(correct))
+    acc = int(correct) / int((len(data.y)*size))
+    print(f'Accuracy: {acc:.4f}')
+
+    return acc, predicted_image
